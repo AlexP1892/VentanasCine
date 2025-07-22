c7938779ad5854e02754da15e6d4f52fdcfb8ea2
AlexP1892 <alex.pogo1805@gmail.com>
2025-07-22 18:27:52 -0500

    Add files via upload

---
+++ ProyectoCine/src/main/java/fis/poo/proyectocine/ProyectoCine.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fis.poo.proyectocine;
+
+/**
+ *
+ * @author User
+ */
+public class ProyectoCine {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> new VentanaPrincipal().setVisible(true));
+    }
+}
